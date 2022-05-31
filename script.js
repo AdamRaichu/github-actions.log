7bbd3a924e712f6498ba1213a6047abd894a7ead
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-31 09:23:05 -0400

    changed method of fetching data

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+$.get("actions.log.yml", function (data) {
+  window.data = data
+});
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("generate").addEventListener("click", function () {
-    $.get("actions.log.yml", function (data) {
-      var unchanged = "?filename=workflows/actions.log.yml&value=";
-      var username = document.getElementById("username").value;
-      var repo = document.getElementById("repo").value;
-      var branch = document.getElementById("branch").value;
-      window.open("https://github.com/" + username + "/" + repo + "/new/" + branch + "/.github/" + unchanged + data, "_blank");
-    });
+    $.get("actions.log.yml", function (data) { });
+    var unchanged = "?filename=workflows/actions.log.yml&value=";
+    var username = document.getElementById("username").value;
+    var repo = document.getElementById("repo").value;
+    var branch = document.getElementById("branch").value;
+    window.open("https://github.com/" + username + "/" + repo + "/new/" + branch + "/.github/" + unchanged + data, "_blank");
   });
 });
