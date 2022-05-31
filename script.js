831b9c490e3884a9be54857f3610d0d82ea158bd
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-31 09:14:06 -0400

    added div#main as well as styles

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("generate").addEventListener("click", function () {
-    $.get("/code/github-actions.log", function (data) {
+    $.get("/github-actions.log/workflows/actions.log.yml", function (data) {
       var unchanged = "?filename=workflows/actions.log.yml&value=";
       var username = document.getElementById("username").value;
       var repo = document.getElementById("repo").value;
       var branch = document.getElementById("branch").value;
-      window.open("https://github.com/" + username + "/" + repo + "/new/" + branch + "/.github/" + unchanged + data, "_self");
+      window.open("https://github.com/" + username + "/" + repo + "/new/" + branch + "/.github/" + unchanged + data, "_blank");
     });
   });
 });
